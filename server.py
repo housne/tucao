616cc23939183a63d718e871fd6b420cac5144da
housne <housne@gmail.com>
2015-04-09 15:19:58 +0800

    fix limit number

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def index():
     page = request.args.get('page')
     if page is None:
         page = 1
-    data = news.list(page=int(page), limit=10)
+    data = news.list(page=int(page), limit=20)
     total = news.count()
     if data is None:
         return jsonResponse(type='404')
