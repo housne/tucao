fd066f91f05c6514c7946d38658cc4db636c6992
housne <housne@gmail.com>
2015-04-22 18:33:54 +0800

    rewrite app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,62 @@
 
 (function(window){
 
-    var articleImg = '';
+    var articleImg = ''; //aritcle image for share
 
+    /*
+        ajax function using promise
+        use it like $http.get.then(fn(url, arguments))
+    */
     var $http =  {
-        'get': function(url, args){
-            return request('GET', url, args);
+
+        get: function(url, args){
+            return this.request('GET', url, args);
         },
-        'post': function(url, args){
-            return request('POST', url, args);
+
+        post: function(url, args){
+            return this.request('POST', url, args);
+        },
+
+        request: function(method, url, args){
+            return new Promise(function(resolve, reject){
+                var xhr = new XMLHttpRequest();
+                var uri = url;
+                if(args) {
+                    uri += '?';
+                    for (key in args) {
+                        if (args.hasOwnProperty(key)) {
+                            uri += encodeURIComponent(key) + '=' + encodeURIComponent(args[key]) + '&';
+                        }
+                    };
+                };
+                if(method === 'POST' || method === 'PUT'){
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                };
+                xhr.addEventListener('readystatechange', function(){
+                    if(xhr.readyState === 4 && xhr.status === 200){
+                        resolve(JSON.parse(xhr.responseText));
+                    }else if(xhr.readyState === 4 && xhr.status !== 200){
+                        reject('request error');
+                    }
+                });
+                xhr.open(method, uri);
+                xhr.send();
+            });
         }
     };
 
+    //simple dom selector
     var $selector = function(selector){
         if(/^\#.+/.test(selector)) return document.getElementById(selector.replace('#', ''));
         if(/^\..+/.test(selector)) return document.getElementsByClassName(selector.replace('.', ''));
     }
 
+    /*
+        evil code, extend element protype
+        I don't want use jquery or zepto or any lib but I want to use the jquery code style
+        so I use this in my private project
+        do not use this in your public project
+    */
     Element.prototype.addClass = function(klass){
         if(this.className.indexOf(klass) !== -1) return this;
         if(!this.className){
@@ -51,7 +91,7 @@
             callback(event, eTarget);
         });
         return this;
-    }
+    };
 
     Element.prototype.setTransLate = function(transform){
         var direct = 'Y';
@@ -62,7 +102,7 @@
         }
         this.setAttribute('style', 'transform:translate' + direct + '('+ transform +'px); -webkit-transform: translate' + direct + '('+ transform +'px);');
         return this;
-    }
+    };
 
     Element.prototype.addEventListeners = function(events, fn){
         var events = events.split(' ');
@@ -70,65 +110,156 @@
         events.forEach(function(event){
             self.addEventListener(event, fn);
         })
-    }
+    };
 
+    var proxy = function(fn, context){
+        args = [].slice.call( arguments, 2 );
+        var proxy = function(){
+            fn.apply(context || this, args.concat([].slice.call(arguments)));
+        };
+        return proxy;
+    };
 
-    function request(method, url, args){
-        return new Promise(function(resolve, reject){
-            var xhr = new XMLHttpRequest();
-            var uri = url;
-            if(args) {
-                uri += '?';
-                for (key in args) {
-                    if (args.hasOwnProperty(key)) {
-                        uri += encodeURIComponent(key) + '=' + encodeURIComponent(args[key]) + '&';
-                    }
+    // share component
+    var shareComponent = {
+
+        container: $selector('#shareContainer'),
+
+        el: $selector('#shareComponent'),
+
+        btn: $selector('#shareBtn'),
+
+        closer: $selector('#hideShare'),
+
+        mask: $selector('#shareMask'),
+
+        item: $selector('#shareBar'),
+
+        hide: function(){
+            var self = this;
+            this.container.removeClass('show');
+            setTimeout(function(){self.el.addClass('hide');}, 300);
+        },
+
+        show: function(){
+            var self = this;
+            this.el.removeClass('hide');
+            setTimeout(function(){self.container.addClass('show');}, 100);
+        },
+
+        share: function(type){
+            var url = '', title = document.title, uri = encodeURIComponent(location.href);
+            switch(type){
+                case 'twitter':
+                    url = 'http://twitter.com/share?text='+ title +'&url=' + uri;
+                    break;
+                case 'facebook':
+                    url = 'http://www.facebook.com/sharer.php?u=' + uri;
+                    break;
+                case 'google':
+                    url = 'https://plus.google.com/share?url=' + uri;
+                    break;
+                case 'weibo':
+                    url = 'http://service.weibo.com/share/share.php?url='+ uri +'&title='+ title +'&pic='+ articleImg;
+                    break;
+            };
+            if(!url) return;
+            window.open(url);
+        },
+
+        init: function(){
+            var self = this;
+            this.btn.addEventListeners('click touchstart', proxy(this.show, this));
+            this.closer.addEventListeners('click touchstart', proxy(this.hide, this));
+            this.mask.addEventListeners('click touchstart', proxy(this.hide, this));
+            this.item.addEventListener('click', function(event){
+                var target = event.target;
+                if(target.tagName.toLowerCase() !== 'button') return;
+                event.preventDefault();
+                var type = target.dataset && target.dataset.type || '';
+                self.share(type);
+                self.hide();
+            });
+        }
+    };
+
+    var route = {
+
+        routes: [
+            {
+                url: /\/$/,
+                controller: initializeIndex
+            },
+            {
+                url: '/news/:id',
+                controller: initializeNews
+            }
+        ],
+
+        resolve: function(){
+            if(window.ga) window.ga('send', 'pageview', window.location.pathname);
+        },
+
+        init: function(){
+            var self = this;
+            var controller = function(index, params){
+                var route = self.routes[index];
+                route.controller(params);
+                if(route.resolve && typeof route.resolve === 'function') route.resolve();
+                self.resolve();
+            };
+            var path = window.location.pathname;
+            for(var i=0, l=this.routes.length; i<l; i++){
+                var url = this.routes[i].url;
+                var params = {};
+                if(typeof url === 'string'){
+                    if(path === url){
+                        controller(i, params);
+                        return;
+                    };
+                    if(typeof url === 'string' && url.indexOf(':') !== -1 && path.indexOf(url.replace(/(.+)\:.+$/, '$1')) !== -1){
+                        var urlKeys = url.split('/'), pathKeys = path.split('/');
+                        urlKeys.forEach(function(key, index){
+                            if(key.indexOf(':') !== -1) params[key.replace(':', '')] = pathKeys[index];
+                        });
+                        controller(i, params);
+                        return;
+                    };
+                };
+                if(Object.prototype.toString.call(url) === '[object RegExp]' && url.test(path)){
+                    controller(i, params);
+                    return;
                 };
             };
-            if(method === 'POST' || method === 'PUT'){
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            };
-            xhr.addEventListener('readystatechange', function(){
-                if(xhr.readyState === 4 && xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText));
-                }else if(xhr.readyState === 4 && xhr.status !== 200){
-                    reject('request error');
-                }
-            });
-            xhr.open(method, uri);
-            xhr.send();
-        });
+        }
+    };
+
+    var pushState = {
+
+        binding: function(){
+           document.getElementsByTagName('body')[0].delegate('click', 'a', function(event, target){
+                var href = target.getAttribute('href') || '';
+                if(!/^\/.*$/.test(href)) return;
+                event.preventDefault();
+                history.pushState({}, document.title, href);
+                route.init();
+            }); 
+        },
+
+        init: function(){
+            this.binding();
+            window.addEventListener('popstate', proxy(route.init, route));
+        }
     };
 
     function initialize(){
-        pushStateRouterHandler();
-        routeHandler();
-        window.addEventListener('popstate', routeHandler)
+        route.init();
+        pushState.init();
         initializeRefresh();
-        shareComponent();
+        shareComponent.init();
     };
 
-    function pushStateRouterHandler(){
-        document.getElementsByTagName('body')[0].delegate('click', 'a', function(event, target){
-            var href = target.getAttribute('href') || '';
-            if(!/^\/.*$/.test(href)) return;
-            event.preventDefault();
-            history.pushState({}, document.title, href);
-            routeHandler();
-        });
-    }
-
-    function routeHandler(){
-        var path = window.location.pathname;
-        if(/^\/news\/\d+/.test(path)){
-            var id = path.replace(/^\/news\/(\d+)/, '$1');
-            initializeNews(id);
-        }else if(/\//.test(path)){
-            initializeIndex();
-        };
-        if(window.ga) window.ga('send', 'pageview', window.location.pathname);
-    }
-
+    
     function initializeRefresh(){
         var page = 1;
         localStorage['page'] = 1;
@@ -176,7 +307,8 @@
         });
     }
 
-    function initializeNews(id){
+    function initializeNews(params){
+        var id = params.id;
         $selector('#indexView').removeClass('loading');
         $http.get('/api/news/'+ id).then(function(response){
             render(response);
@@ -198,48 +330,6 @@
         };
     }
 
-    function shareComponent(){
-        var hideShareComponent = function(){
-            $selector('#shareContainer').removeClass('show');
-            setTimeout(function(){$selector('#shareComponent').addClass('hide');}, 300);
-        };
-        $selector('#shareBtn').addEventListeners('click touchstart', function(event){
-            event.preventDefault();
-            $selector('#shareComponent').removeClass('hide');
-            setTimeout(function(){$selector('#shareContainer').addClass('show');}, 100);
-        });
-        $selector('#hideShare').addEventListeners('click touchstart', hideShareComponent);
-        $selector('#shareMask').addEventListeners('click touchstart', hideShareComponent);
-
-        var share = function(type){
-            var url = '', title = document.title, uri = encodeURIComponent(location.href);
-            switch(type){
-                case 'twitter':
-                    url = 'http://twitter.com/share?text='+ title +'&url=' + uri;
-                    break;
-                case 'facebook':
-                    url = 'http://www.facebook.com/sharer.php?u=' + uri;
-                    break;
-                case 'google':
-                    url = 'https://plus.google.com/share?url=' + uri;
-                    break;
-                case 'weibo':
-                    url = 'http://service.weibo.com/share/share.php?url='+ uri +'&title='+ title +'&pic='+ articleImg;
-                    break;
-            };
-            if(!url) return;
-            window.open(url);
-        };
-
-        $selector('#shareBar').addEventListener('click', function(event){
-            var target = event.target;
-            if(target.tagName.toLowerCase() !== 'button') return;
-            event.preventDefault();
-            var type = target.dataset && target.dataset.type || '';
-            share(type);
-            hideShareComponent();
-        })
-    }
 
     function convertTimestampToLoaleDateString(result){
 
